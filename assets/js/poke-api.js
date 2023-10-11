72f23a125fc34ef2aaeb4773e7fba5cfc4ca25fa
Tamires Dib <tamiresdib6@gmail.com>
2023-10-10 22:42:14 -0300

    adding request list for each pokemon

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,12 +1,19 @@
 const pokeApi = {};
 
-pokeApi.getPokemons = () => {
-  const offset = 10;
-  const limit = 10;
-  const url =
-    "https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}";
-  return fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .catch((error) => console.log(error));
+pokeApi.getPokemonsDetail = (pokemon) => {
+  return fetch(pokemon.url).then((response) => response.json());
 };
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
+  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+};
+
+return fetch(url)
+  .then((response) => response.json())
+  .then((jsonBody) => jsonBody.results)
+  .then((pokemons) => pokemons.map(pokeApi.getPokemonsDetail))
+  .then((detailRequests) => Promise.all(detailRequests))
+  .then((getPokemonsDetail) => {
+    debugger;
+    console.log(getPokemonsDetail);
+  });
